67138bb12253a6bb0012eb57488d19c50af55499
Michael Bestas <mkbestas@gmail.com>
2025-03-06 23:31:19 +0200

    Include missing vibrator sepolicy
    Change-Id: Iac3a36e884dc1747d4fd0a60b418ffbf3779ffdb

--- sepolicy/lynx-sepolicy.mk
+++ sepolicy/lynx-sepolicy.mk
@@ -1,3 +1,6 @@
 # sepolicy that are shared among devices using whitechapel
 BOARD_SEPOLICY_DIRS += device/google/lynx/sepolicy/vendor
 BOARD_SEPOLICY_DIRS += device/google/lynx/sepolicy/tracking_denials
+
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/vibrator/common
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/vibrator/cs40l26
